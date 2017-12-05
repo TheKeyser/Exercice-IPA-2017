14afd0adc8d92a897e99b6ab862f91a62fa725c2
TheKeyser <34234308+TheKeyser@users.noreply.github.com>
2017-12-05 10:48:22 +0100

    Le changement premier essai

--- Pascal/Série 11/TraitementImages.pas
+++ Pascal/Série 11/TraitementImages.pas
@@ -6,10 +6,12 @@ procedure convertirenndg(var Image: TImage);
   var
     i : integer;
     j : integer;
+    g : integer;
 begin
   for i:= 0 to 511 do begin
     for j:= 0 to 511 do begin
-      writeln(Image.pixels[i,j].green)
+      g := trunc(0.3*Image.pixels[i,j].red + 0.59*Image.pixels[i,j].green + 0.11*Image.pixels[i,j].blue);
+      writeln(g);
     end;
   end;
 
@@ -17,6 +19,6 @@ end;
 begin
   ImagesLena := open_image('lenna.png',512,512);
   convertirenndg(ImagesLena);
-  writeln(ImagesLena.pixels[0,0].green);
+
   readln;
 end.
