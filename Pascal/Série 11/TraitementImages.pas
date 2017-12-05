128d86acfbc7ef690723d321cd0d8b11866e1cb7
TheKeyser <34234308+TheKeyser@users.noreply.github.com>
2017-12-05 10:35:08 +0100

    Série 11 premier essai

--- Pascal/Série 11/TraitementImages.pas
+++ Pascal/Série 11/TraitementImages.pas
@@ -3,9 +3,16 @@ Uses image_utils;
  var
     ImagesLena: TImage;
 procedure convertirenndg(var Image: TImage);
-
+  var
+    i : integer;
+    j : integer;
 begin
-  Image.pixels[0,0].green := 0;
+  for i:= 0 to 511 do begin
+    for j:= 0 to 511 do begin
+      writeln(Image.pixels[i,j].green)
+    end;
+  end;
+
 end;
 begin
   ImagesLena := open_image('lenna.png',512,512);
