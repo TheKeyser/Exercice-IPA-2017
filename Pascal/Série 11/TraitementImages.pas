b84bcfa2ce9962cfed652732999f9adf14988fe5
cauchy74 <quentin@defossez.ch>
2017-12-05 10:11:25 +0100

    ajout fonction

--- Pascal/Série 11/TraitementImages.pas
+++ Pascal/Série 11/TraitementImages.pas
@@ -2,12 +2,14 @@ program TraitementImages;
 Uses image_utils;
  var
     ImagesLena: TImage;
-procedure convertirenndg();
+procedure convertirenndg(var Image: TImage);
+
 begin
+  Image.pixels[0,0].green := 0;
 end;
 begin
   ImagesLena := open_image('lenna.png',512,512);
-  ImagesLena.pixels[0,0].green := 0;
+  convertirenndg(ImagesLena);
   writeln(ImagesLena.pixels[0,0].green);
   readln;
 end.
